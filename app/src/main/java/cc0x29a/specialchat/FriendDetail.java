25352ce71604b52ee026b28e9b00ff86f9ab86c3
Zhong Wenliang <cuberwenliang@163.com>
2020-03-12 14:43:55 +0800

    New changelist

--- app/src/main/java/cc0x29a/specialchat/FriendDetail.java
+++ app/src/main/java/cc0x29a/specialchat/FriendDetail.java
@@ -9,6 +9,6 @@ public class FriendDetail extends AppCompatActivity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_contacts_detail);//todo error!!
+		setContentView(R.layout.activity_contacts_detail);
 	}
 }
