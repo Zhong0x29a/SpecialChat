4321fe322e7e08881b5d5e1e4cb10947918bc028
Zhong Wenliang <cuberwenliang@163.com>
2020-03-13 12:21:40 +0800

    New changelist

--- app/src/main/java/cc0x29a/specialchat/ChatActivity.java
+++ app/src/main/java/cc0x29a/specialchat/ChatActivity.java
@@ -1,16 +1,13 @@
 package cc0x29a.specialchat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.Toast;
-
-import java.util.prefs.Preferences;
 
 public class ChatActivity extends AppCompatActivity{
 	
@@ -35,30 +32,41 @@ public class ChatActivity extends AppCompatActivity{
 			SharedPreferences preferences=getSharedPreferences("user_info",MODE_PRIVATE);
 			String my_id=preferences.getString("user_id",null);
 			
-			ChatRecordAdapter chatRecordAdapter=new ChatRecordAdapter(ChatActivity.this);
-			chatRecordAdapter.count=Integer.parseInt(record[0][0]);
-			chatRecordAdapter.messages=record;
-			chatRecordAdapter.my_id=my_id;
+			RecyclerView recyclerView=findViewById(R.id.chatWindow_recyclerView);
+			LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+			recyclerView.setLayoutManager(layoutManager);
 			
-			ListView chat_listView=findViewById(R.id.chatWindow_listView);
-			chat_listView.setAdapter(chatRecordAdapter);
+			ChatWindowAdapter adapter=new ChatWindowAdapter(record);
+			adapter.count=Integer.parseInt(record[0][0]);
+			adapter.my_id=my_id;
+			recyclerView.setAdapter(adapter);
+			recyclerView.setItemAnimator(new DefaultItemAnimator());
 			
-			chat_listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-				@Override
-				public void onItemClick(AdapterView<?> parent,View view,int position,long id){
-					position++;
-					// Noting to do here.
-				}
-			});
 			
-			chat_listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
-				@Override
-				public boolean onItemLongClick(AdapterView<?> parent,View view,int position,long id){
-					position++;
-					// todo add a menu to delete msg here.
-					return true;
-				}
-			});
+			//			ChatWindowAdapter chatRecordAdapter=new ChatWindowAdapter(ChatActivity.this);
+//			chatRecordAdapter.count=Integer.parseInt(record[0][0]);
+//			chatRecordAdapter.messages=record;
+//			chatRecordAdapter.my_id=my_id;
+//
+//			ListView chat_listView=findViewById(R.id.chatWindow_listView);
+//			chat_listView.setAdapter(chatRecordAdapter);
+			
+//			chat_listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+//				@Override
+//				public void onItemClick(AdapterView<?> parent,View view,int position,long id){
+//					position++;
+//					// Noting to do here.
+//				}
+//			});
+//
+//			chat_listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+//				@Override
+//				public boolean onItemLongClick(AdapterView<?> parent,View view,int position,long id){
+//					position++;
+//					// todo add a menu to delete msg here.
+//					return true;
+//				}
+//			});
 			//chat_listView.scrollTo(0,0);
 		}else{
 			Toast.makeText(ChatActivity.this,"ERROR! ",Toast.LENGTH_LONG).show();
@@ -74,7 +82,5 @@ public class ChatActivity extends AppCompatActivity{
 	@Override
 	protected void onStart(){
 		super.onStart();
-		findViewById(R.id.chatWindow_listView).scrollTo(0,findViewById(R.id.chatWindow_listView).getBottom());
-		
 	}
 }
--- app/src/main/java/cc0x29a/specialchat/ChatWindowAdapter.java
+++ app/src/main/java/cc0x29a/specialchat/ChatWindowAdapter.java
@@ -1,14 +1,91 @@
 package cc0x29a.specialchat;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-//todo complete this class!!!
+import androidx.recyclerview.widget.RecyclerView;
+
+/*
+ * Adapter for R.id.chatWindow_listView.
+ * To show chat records
+ */
+
+
+public class ChatWindowAdapter extends RecyclerView.Adapter<ChatWindowAdapter.VH>{
+	String my_id;
+	private String[][] messages;
+	int count;
+	
+	static class VH extends RecyclerView.ViewHolder{
+		final TextView chat_msg_tv;
+		final LinearLayout chat_msg_container;
+		VH(View v) {
+			super(v);
+			chat_msg_tv= v.findViewById(R.id.chat_msg_content);
+			chat_msg_container=v.findViewById(R.id.chat_msg_container);
+		}
+	}
+	
+	public ChatWindowAdapter(String[][] data) {
+		this.messages = data;
+	}
+	
+	//③ 在Adapter中实现3个方法
+	@Override
+	public void onBindViewHolder(VH holder, int position) {
+		int index=position+1;
+		holder.chat_msg_tv.setText(messages[index][4]);
+		if(messages[index][1]!=null && messages[index][1].equals(my_id)){
+			holder.chat_msg_container.setBackgroundResource(R.drawable.my_msg_style);
+		}else{
+			holder.chat_msg_container.setBackgroundResource(R.drawable.ta_msg_style);
+		}
+		holder.itemView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//item 点击事件
+			}
+		});
+	}
+	
+	@Override
+	public int getItemCount() {
+		return this.count;
+	}
+	
+	@Override
+	public VH onCreateViewHolder(ViewGroup parent,int viewType) {
+		//LayoutInflater.from指定写法
+		View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_msg_item, parent, false);
+		return new VH(v);
+	}
+}
+
+
+
+
+/*
+
 public class ChatWindowAdapter extends BaseAdapter{
+	private LayoutInflater layoutInflater;
+	String my_id;
+	String[][] messages;
+	int count;
+	
+	ChatWindowAdapter(Context context){
+		layoutInflater=LayoutInflater.from(context);
+	}
+	
+	static class ViewHolder{
+		TextView msg;
+	}
+	
 	@Override
 	public int getCount(){
-		return 0;
+		return this.count;
 	}
 	
 	@Override
@@ -21,8 +98,32 @@ public class ChatWindowAdapter extends BaseAdapter{
 		return 0;
 	}
 	
+	@SuppressLint("InflateParams")
 	@Override
 	public View getView(int position,View convertView,ViewGroup parent){
-		return null;
+		ViewHolder holder;
+		position++;
+		if(convertView==null){
+			if(messages[position][1].equals(my_id)){
+//			if(messages[position][1].equals("2950")){
+				
+				convertView=layoutInflater.inflate(R.layout.chat_my_msg,null);
+				holder=new ViewHolder();
+				holder.msg=convertView.findViewById(R.id.chat_my_msg);
+			}else{
+				convertView=layoutInflater.inflate(R.layout.chat_ta_msg,null);
+				holder=new ViewHolder();
+				holder.msg=convertView.findViewById(R.id.chat_ta_msg);
+			}
+			
+			convertView.setTag(holder);
+		}else{
+			holder=(ViewHolder) convertView.getTag();
+		}
+		
+		holder.msg.setText(messages[position][4]);
+		return convertView;
 	}
 }
+
+*/
